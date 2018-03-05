50b8de9e578b89481f82350af8898f266338fae2
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2018-03-05 17:22:16 +0900

    [add] 双方向通信できた！

--- app.js
+++ app.js
@@ -1,46 +1,63 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req, res){
+	res.send(`
+		<!doctype html>
+		<html>
+			<head>
+				<title>Socket.IO chat</title>
+				<style>
+					* { margin: 0; padding: 0; box-sizing: border-box; }
+					body { font: 13px Helvetica, Arial; }
+					form { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }
+					form input { border: 0; padding: 10px; width: 90%; margin-right: .5%; }
+					form button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }
+					#messages { list-style-type: none; margin: 0; padding: 0; }
+					#messages li { padding: 5px 10px; }
+					#messages li:nth-child(odd) { background: #eee; }
+				</style>
+			</head>
+			<body>
+				<ul id="messages"></ul>
+				<form action="">
+					<input id="m" autocomplete="off" /><button>Send</button>
+				</form>
+				<script src="/socket.io/socket.io.js"></script>
+				<script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+				<script>
+					$(function () {
+						var socket = io();
+						$('form').submit(function(){
+							socket.emit('chat message', $('#m').val());
+							$('#m').val('');
+							return false;
+						});
+						socket.on('chat message', function(msg){
+							console.log(msg)
+							$('#messages').append($('<li>').text(msg));
+						});
+					});
+				</script>
+			</body>
+		</html>
+	`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+http.listen(3001, function(){
+	console.log('listening on *:3001');
 });
 
 module.exports = app;
